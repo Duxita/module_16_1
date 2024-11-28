114c51b133f18b2490490b7c9057b9a3c5e6000b
Duxita <Duxita@mail.ru>
2024-11-28 21:51:39 +0300

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Главная страница"
+@app.get("/user/admin")
+def admin():
+    return "Вы вошли как администратор"
+
+@app.get("/user/{user_id}")
+def user(user_id: int):
+    return f"Вы вошли как пользователь № {user_id}"
+
+@app.get("/user")
+def user_info(username: str, age: int):
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
+
